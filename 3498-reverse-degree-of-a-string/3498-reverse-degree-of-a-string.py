16f436a4c8136317cc10f3ff2f2cee0976c0697f
Vedant Jaiswal <jaiswalvedant2004@gmail.com>
2026-09-20 10:55:23 +0530

    Time: 3 ms (97.65%), Space: 19.2 MB (55.57%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -2,10 +2,7 @@ class Solution:
     def reverseDegree(self, s: str) -> int:
         sumi=0
         for i in range(0,len(s)):
-            x=ord(s[i])
-            l=x-97
-            indi=26-l
-            sumi=sumi+indi*(i+1)
+            sumi=sumi+(26-((ord(s[i]))-97))*(i+1)
         return sumi
 
         
